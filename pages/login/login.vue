66b8040fa2ac871dce93357ccffc5f3352e1b2b3
liu guoliang <liuguoliang@hanhou.cn>
2022-09-28 20:46:25 +0800

    nav

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,5 +1,8 @@
 <template>
 	<view class="container">
+		<view class="nav">
+			<image @tap="handleBack()" class="nav-left" src="../../static/Frame 1000006270.png" mode=""></image>
+		</view>
 		<view class="header">
 			<image class="head-img" src="../../static/logo.png" alt=""></image>
 		</view>
@@ -58,6 +61,20 @@
 			};
 		},
 		methods: {
+			handleBack() {
+
+				let currentRoutes = getCurrentPages(); // 获取当前打开过的页面路由数组
+				console.log(currentRoutes)
+				if (currentRoutes.length === 1) {
+					uni.redirectTo({
+						url: '/pages/index/index'
+					})
+				} else {
+					uni.navigateBack({
+						delta: 1, //返回层数，2则上上页
+					})
+				}
+			},
 			// 查看协议
 			handView(url) {
 				uni.navigateTo({
@@ -190,6 +207,28 @@
 
 <style lang="scss" scoped>
 	.container {
+		padding-top: 88rpx;
+
+		.nav {
+			position: fixed;
+			top: 0;
+			left: 0;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			width: 100%;
+			height: 88rpx;
+			background-color: #0D0D0D;
+			z-index: 10;
+
+			.nav-left {
+				position: absolute;
+				top: 28rpx;
+				left: 28rpx;
+				width: 48rpx;
+				height: 48rpx;
+			}
+		}
 
 		.header {
 			display: flex;
