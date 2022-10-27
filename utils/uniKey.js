80fa17d0f08767a8e79092b9bd1e9334a8908ebb
liu guoliang <liuguoliang@hanhou.cn>
2022-10-27 14:14:39 +0800

    1800

--- utils/uniKey.js
+++ utils/uniKey.js
@@ -8,17 +8,17 @@ const development = {
 }
 
 const prod = {
-	ENV: 'test', //打包到测试环境
-	BASE_URL: 'https://c.yuanyinfu.com',
-	APP_DOWNLOAD_URL: 'https://share.yuanyinfu.com/h5/app',
-	appURL: 'https://c.yuanyinfu.com', //本web地址
-	appId: 'wxc71083a78fa68d77', //公众号id
-
-	// ENV: 'prod', //打包到生产环境
-	// BASE_URL: 'https://yyf-h5.hanhoukeji.com',
+	// ENV: 'test', //打包到测试环境
+	// BASE_URL: 'https://c.yuanyinfu.com',
 	// APP_DOWNLOAD_URL: 'https://share.yuanyinfu.com/h5/app',
-	// appURL: 'https://yyf-h5.hanhoukeji.com',
-	// appId: 'wxc71083a78fa68d77'
+	// appURL: 'https://c.yuanyinfu.com', //本web地址
+	// appId: 'wxc71083a78fa68d77', //公众号id
+
+	ENV: 'prod', //打包到生产环境
+	BASE_URL: 'https://yyf-h5.hanhoukeji.com',
+	APP_DOWNLOAD_URL: 'https://share.yuanyinfu.com/h5/app',
+	appURL: 'https://yyf-h5.hanhoukeji.com',
+	appId: 'wxc71083a78fa68d77'
 }
 let compileConfig = null
 if (processName === 'development') {
