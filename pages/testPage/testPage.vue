3f02724aa2826ca5fd578cfd4f63bce4726252af
liu guoliang <liuguoliang@hanhou.cn>
2022-11-03 19:11:23 +0800

    testpage

--- pages/testPage/testPage.vue
+++ pages/testPage/testPage.vue
@@ -38,7 +38,7 @@
 
 		},
 		mounted() {
-			this.wxReady()
+			// this.wxReady()
 			this.init()
 		},
 		methods: {
@@ -58,6 +58,14 @@
 				const res = await this.$post(h5_collections_wechat_get_jssdk, {
 					url: window.location.origin
 				})
+				wx.config({
+					debug: true, // 开启调试模式,调用的所有 api 的返回值会在客户端 alert 出来，若要查看传入的参数，可以在 pc 端打开，参数信息会通过 log 打出，仅在 pc 端时才会打印。
+					appId: res.data.app_id, // 必填，公众号的唯一标识
+					timestamp: res.data.timestamp, // 必填，生成签名的时间戳
+					nonceStr: res.data.nonce_str, // 必填，生成签名的随机串
+					signature: res.data.signature, // 必填，签名
+					jsApiList: [] // 必填，需要使用的 JS 接口列表
+				});
 
 			}
 
