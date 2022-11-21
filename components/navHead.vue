27c5441a1b366e3de275f92fee9776d2ed728863
liu guoliang <liuguoliang@hanhou.cn>
2022-11-21 17:33:40 +0800

    nav

--- components/navHead.vue
+++ components/navHead.vue
@@ -1,5 +1,5 @@
 <template>
-	<view class="slots">
+	<view :class="$store.state.user.inApp?'slots':'web'">
 		<view class="nav">
 			<image v-show="left" @tap="navBack()" class="nav-left" src="../static/navLeft.png" mode=""></image>
 			<text class="nav-center">{{title}}</text>
@@ -69,55 +69,110 @@
 <style lang="scss" scoped>
 	.slots {
 		height: 148rpx;
-	}
 
-	.nav {
-		position: fixed;
-		top: 0;
-		left: 0;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		padding-top: 60rpx;
-		height: 88rpx;
-		background-color: #0D0D0D;
-		z-index: 10;
-
-		.nav-left {
-			position: absolute;
-			top: 80rpx;
-			left: 28rpx;
-			width: 48rpx;
-			height: 48rpx;
-		}
-
-		.nav-center {
-			width: 100%;
-			height: 88rpx;
-			color: #DDDDDD;
-			font-size: 30rpx;
-			font-family: 'PingFang SC';
-			font-style: normal;
-			font-weight: 400;
-			text-align: center;
-			line-height: 88rpx;
-		}
-
-		.nav-right {
-			position: absolute;
-			top: 60rpx;
-			right: 28rpx;
+		.nav {
+			position: fixed;
+			top: 0;
+			left: 0;
 			display: flex;
 			align-items: center;
-			justify-content: flex-end;
-			width: 88rpx;
+			justify-content: center;
+			width: 100%;
+			padding-top: 60rpx;
 			height: 88rpx;
+			background-color: #0D0D0D;
+			z-index: 10;
 
-			image {
-				background-color: red;
-				width: 48rpx !important;
+			.nav-left {
+				position: absolute;
+				top: 80rpx;
+				left: 28rpx;
+				width: 48rpx;
 				height: 48rpx;
+			}
+
+			.nav-center {
+				width: 100%;
+				height: 88rpx;
+				color: #DDDDDD;
+				font-size: 30rpx;
+				font-family: 'PingFang SC';
+				font-style: normal;
+				font-weight: 400;
+				text-align: center;
+				line-height: 88rpx;
+			}
+
+			.nav-right {
+				position: absolute;
+				top: 60rpx;
+				right: 28rpx;
+				display: flex;
+				align-items: center;
+				justify-content: flex-end;
+				width: 88rpx;
+				height: 88rpx;
+
+				image {
+					background-color: red;
+					width: 48rpx !important;
+					height: 48rpx;
+				}
+			}
+		}
+	}
+
+
+	.web {
+		height: 88rpx;
+
+		.nav {
+			position: fixed;
+			top: 0;
+			left: 0;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			width: 100%;
+			height: 88rpx;
+			background-color: #0D0D0D;
+			z-index: 10;
+
+			.nav-left {
+				position: absolute;
+				top: 20rpx;
+				left: 28rpx;
+				width: 48rpx;
+				height: 48rpx;
+			}
+
+			.nav-center {
+				width: 100%;
+				height: 88rpx;
+				color: #DDDDDD;
+				font-size: 30rpx;
+				font-family: 'PingFang SC';
+				font-style: normal;
+				font-weight: 400;
+				text-align: center;
+				line-height: 88rpx;
+			}
+
+			.nav-right {
+				position: absolute;
+				top: 0rpx;
+				right: 28rpx;
+				display: flex;
+				align-items: center;
+				justify-content: flex-end;
+				width: 88rpx;
+				height: 88rpx;
+
+				image {
+					background-color: red;
+					width: 48rpx !important;
+					height: 48rpx;
+				}
 			}
 		}
 	}
