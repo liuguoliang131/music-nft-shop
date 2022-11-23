f7b145429ec4b3144e1ca4fd72bb2789732be0be
liu guoliang <liuguoliang@hanhou.cn>
2022-11-23 17:48:54 +0800

    转赠在H5不显示

--- pages/albumRecordsDetail/albumRecordsDetail.vue
+++ pages/albumRecordsDetail/albumRecordsDetail.vue
@@ -124,10 +124,16 @@
 		<view class="h116">
 
 		</view>
-		<view class="fixed-bottom">
+		<view class="fixed-bottom" v-if="$store.state.user.inApp">
 			<view class="zhuanzeng" @tap="handZhuanZeng">
 				转赠
 			</view>
+			<view class="tingge" @tap="handTingGe">
+				<image src="../../static/play.png" mode=""></image>
+				欣赏专辑
+			</view>
+		</view>
+		<view class="fixed-bottom kelian" v-else>
 			<view class="tingge" @tap="handTingGe">
 				<image src="../../static/play.png" mode=""></image>
 				欣赏专辑
@@ -563,6 +569,30 @@
 			.tingge {
 				background: #FFFFFF;
 				width: 332rpx;
+				height: 96rpx;
+				border-radius: 48rpx;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				font-family: 'PingFang SC';
+				font-style: normal;
+				font-weight: 500;
+				font-size: 32rpx;
+				line-height: 96px;
+				color: #532609;
+
+				image {
+					margin-right: 8rpx;
+					width: 40rpx;
+					height: 40rpx;
+				}
+			}
+		}
+
+		.kelian {
+			.tingge {
+				background: #FFFFFF;
+				width: 686rpx;
 				height: 96rpx;
 				border-radius: 48rpx;
 				display: flex;
