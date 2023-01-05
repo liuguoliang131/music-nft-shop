6a9949cba91b7018cfa37f0ece8b0e932ba8dc9d
liu guoliang <liuguoliang@hanhou.cn>
2023-01-05 19:03:41 +0800

    transition

---
+++ pages/transition/transition.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="container">
+		<nav-head></nav-head>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.container {
+		padding: 0;
+	}
+</style>
